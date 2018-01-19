5936fd7ef6606e826df90b5b3d3c34749e107a95
James Kirsch <jameskirschaf@gmail.com>
2018-01-18 19:38:39 -0800

    React | react-intro - James Kirsch

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const luckyNumber = () => Math.ceil(Math.random()*1000);
+
+const greeting = userObj => (
+    <div>
+        <h1>Welcome {userObj.name}</h1>
+        <h2 className="text-muted">Your lucky number is: {userObj.luckyNumber}</h2>
+    </div>
+);
+
+const user = {
+        name : "Jake",
+        luckyNumber : luckyNumber()
+};
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
